4f11c0956848ac7cd63fa7a89443b7713e5ca905
madreher <dreher.matthieu@gmail.com>
2024-06-25 01:19:36 -0400

    Start from frame (#31)
    * First implementation of input generation using frames instead of original state.
    
    * Update the launch simulatin function to be able to start from a given frame. Reset the molecular viewer also accordingly to remove all the other frames.

---
+++ frontend/static/js/project_utils.js
@@ -0,0 +1,26 @@
+var radahnProjectUtils = {};
+
+radahnProjectUtils.viewerToXYZ = function(viewer, frameIndex)
+{
+    if(frameIndex < 0 || frameIndex >= viewer.getModel().getNumFrames())
+    {
+        console.error("Frame index requested out of bounds: " + frameIndex);
+        return "";
+    }
+    let frame = viewer.getModel().frames[frameIndex];
+
+    if(frame.length == 0)
+    {
+        console.error("Frame is empty: " + frameIndex);
+        return "";
+    }
+
+    let xyz = "";
+    xyz += frame.length + "\n";
+    xyz += "Frame: " + frameIndex + "\n";
+    for(let i = 0; i < frame.length; i++)
+    {
+        xyz += frame[i].elem + " " + frame[i].x + " " + frame[i].y + " " + frame[i].z + "\n";
+    }
+    return xyz;
+}
