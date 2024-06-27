122d58b4fe41e7089796e42fd6e20852694fa764
Matthieu Dreher <dreher.matthieu@gmail.com>
2024-06-27 03:59:49 -0400

    Add script to load necessary libs

---
+++ docker/ubuntu/activate.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/app/radahn/radahn/build/install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/app/radahn/opt/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/app/radahn/godrick/build/install/lib:$LD_LIBRARY_PATH
