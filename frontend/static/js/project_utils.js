e7cbf79cb643f08674b48e25ac7b2910b20f1696
madreher <dreher.matthieu@gmail.com>
2024-06-30 22:46:09 -0400

    Clear the trajectory when starting a new simulation from the original state. The trajectory was being appended instead of cleared. (#36)

--- frontend/static/js/project_utils.js
+++ frontend/static/js/project_utils.js
@@ -24,3 +24,28 @@ radahnProjectUtils.viewerToXYZ = function(viewer, frameIndex)
     }
     return xyz;
 }
+
+radahnProjectUtils.viewerToArray = function(viewer, frameIndex)
+{
+    if(frameIndex < 0 || frameIndex >= viewer.getModel().getNumFrames())
+    {
+        console.error("Frame index requested out of bounds: " + frameIndex);
+        return "";
+    }
+    let frame = viewer.getModel().frames[frameIndex];
+
+    if(frame.length == 0)
+    {
+        console.error("Frame is empty: " + frameIndex);
+        return "";
+    }
+
+    let positions = []
+    for(let i = 0; i < frame.length; i++)
+    {
+        positions.push([frame[i].x, frame[i].y, frame[i].z]);
+    }
+
+    return positions;
+
+}
